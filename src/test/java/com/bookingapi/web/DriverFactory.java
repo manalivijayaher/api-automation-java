001b3449783197a96badfa014562036610ade102
vijayaher0211 <58352950+vijayaher0211@users.noreply.github.com>
2026-09-03 12:10:13 +0400

    driver factory changes

--- src/test/java/com/bookingapi/web/DriverFactory.java
+++ src/test/java/com/bookingapi/web/DriverFactory.java
@@ -11,14 +11,20 @@ public class DriverFactory {
 	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 	
 	public static WebDriver getDriver() {
-		if(driver.get()==null) {
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
-			driver.set(new ChromeDriver(options));
-		}
-		return driver.get();
+	    if (driver.get() == null) {
+	        WebDriverManager.chromedriver().setup();
+	        ChromeOptions options = new ChromeOptions();
+
+	        if (System.getenv("CI") != null) {
+	            options.addArguments("--headless=new");
+	            options.addArguments("--no-sandbox");
+	            options.addArguments("--disable-dev-shm-usage");
+	        }
+
+	        driver.set(new ChromeDriver(options));
+	    }
+	    return driver.get();
 	}
-	
 	public static void quitDriver() {
 		if(driver.get() != null) {
 			driver.get().quit();
